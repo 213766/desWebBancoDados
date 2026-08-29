ba33b9ca4e99e86f4b2939c7e65b2a51ea7cca70
213766 <213766@upf.br>
2026-08-29 11:43:45 -0300

    finalizado a aula 10

--- aula10/desafio.php
+++ aula10/desafio.php
@@ -1,6 +1,16 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'ua10')
 
-    $sql = 
+    $produto = "Maçã";
+    $preco = 8.79;
+
+    $conn = mysqli_connect('localhost', 'root', '', 'ua10');
+
+    $sql = "INSERT INTO produtos (descricao_produto, preco_produto) VALUES (?, ?)";
+
+    $stmt = mysqli_prepare($conn, $sql);
+
+    mysqli_stmt_bind_param($stmt, "sd", $produto, $preco);
+ 
+    mysqli_stmt_execute($stmt);
 
 ?>
